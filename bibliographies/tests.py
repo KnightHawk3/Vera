6af6d3daffa43dfd74614b1b8c11902cf9cc7776
Declan Kelly <declan@KnightHawk3.com>
2013-04-28 18:49:44 +1000

    Removed tests

--- bibliographies/tests.py
+++ bibliographies/tests.py
@@ -4,10 +4,3 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
-
-import datetime
-
-from django.utils import timezone
-from django.test import TestCase
-from django.core.urlresolvers import reverse
