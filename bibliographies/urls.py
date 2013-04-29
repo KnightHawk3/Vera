0b8ad3a10d907c55c94421af86dad0ecd6e153c2
Declan Kelly <declan@KnightHawk3.com>
2013-04-30 09:26:43 +1000

    Fixed bug #1

--- bibliographies/urls.py
+++ bibliographies/urls.py
@@ -1,18 +1,13 @@
 from django.conf.urls import patterns, url
 from django.views.generic import DetailView, TemplateView
-from django.utils import timezone
 from bibliographies.models import Bibliography
 
 urlpatterns = patterns('',
     url(r'^$',
         TemplateView.as_view(template_name='bibliographies/index.html'),
         name='index'),
-    #url(r'^login/$',
-    #    TemplateView.as_view(template_name='bibliographies/login.html'),
-    #    name='login'),
     url(r'^(?P<pk>\d+)/$',
         DetailView.as_view(
-            queryset=Bibliography.objects.filter(create_date__lte=timezone.now),
             model=Bibliography,
             template_name='bibliographies/detail.html'),
         name='detail'),
